728222e16e4a50105c2967c79110423339b3bf28
ivycheung7 <ischeung7@Gmail.com>
2015-09-16 12:40:12 -0400

    Update cats.js

--- cats.js
+++ cats.js
@@ -1,3 +1,3 @@
 function showShaqCat(){
-    $('#sorry').delay(5000).hide().html('<img url="https://cloud.githubusercontent.com/assets/13770844/9885928/c95eda1e-5bb5-11e5-9c41-9f3c88ace204.gif" alt="Cat.gif" height:860 width:600>').fadeIn();
+    $('#sorry').delay(5000).hide().html('<img src="https://cloud.githubusercontent.com/assets/13770844/9885928/c95eda1e-5bb5-11e5-9c41-9f3c88ace204.gif" alt="Cat.gif" height:860 width:600>').fadeIn();
 }
