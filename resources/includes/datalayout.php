5ec2360da4f51defc97fe5f0d4abef3db999972d
Ivy Cheung <scheung7@hotmail.com>
2016-04-13 23:22:28 -0400

    github is telling me to commit so i must commit

--- resources/includes/datalayout.php
+++ resources/includes/datalayout.php
@@ -12,12 +12,13 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 	<!--  Javascript Files -->
     <!--  For some reason, this does not work :: <script type="text/javascript" src="../resources/js/dogsRUs.js"></script>-->
 	<script type="text/javascript" src="dogsRUs.js"></script>
+	<!-- JQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 	
 	<title>Dogs 'R Us</title>
 	</head>
 	<body>
-	
-    <!-- Navbar -->
+	<!-- Navbar -->
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container"> 
             <div class="navbar-header page-scroll"> 
@@ -41,22 +42,35 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
     </nav>
         
 	<div id = "layout">
-		<div class="row">
-			<div class= "col-md-4" name="sarah" onclick="description('sarah')">
+		<div class="row" id="smallDogs">
+			<div class= "col-md-3" id="sortList">
+				<div class ="row" id="size">
+					<h4>Refine by</h4>
+					<h5>Size<br>
+					<input type="checkbox" id="small" value="Small" onclick="refine()"> Small<br>
+					<input type="checkbox" id="medium" value="Medium" onclick="refine()"> Medium<br>
+					<input type="checkbox" id="large" value="Large" onclick="refine()"> Large<br>
+					</h5>
+				</div>
+				<div class ="row">
+				
+				</div>
+			</div>
+			<div class= "col-md-3" name="sarah" onclick="description('sarah')">
 				<img src="http://www.cutestpaw.com/wp-content/uploads/2011/11/cute-puppy1.jpg" >
 				<h4>Sarah</h4>
 				<p>Lived in tiny cups for 90% of her life.
 					<br><em>One mile away</em>
 				</p>
 			</div>
-			<div class= "col-md-4" >
+			<div class= "col-md-3" >
 				<img src="http://i.imgur.com/qNoTX.jpg">
 				<h4>Bruno</h4>
 				<p>Tilts for days
 					<br><em>1.5 miles away</em>
 				</p>
 			</div>
-			<div class= "col-md-4" name="willow" onclick="description('willow')">
+			<div class= "col-md-3" name="willow" onclick="description('willow')">
 				<img src="http://i.imgur.com/wOvig8T.gif">
 				<h4>Willow</h4>
 				<p>Will not stay still, need energetic owner
@@ -64,22 +78,25 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 				</p>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-4">
+		<div class="row" id="largeDogs">
+			<div class= "col-md-3" >
+
+			</div>
+			<div class="col-md-3">
 				<img src="http://i.imgur.com/oT4qU.jpg">
 				<h4>Britnee</h4>
 				<p>Very good replacement for comforter
 					<br><em>0.5 miles away</em>
 				</p>
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-3">
 				<img src="http://i.imgur.com/yyyyPUq.jpg">
 				<h4>Godfrey</h4>
 				<p>100% Guaranteed Alive, sleeps in warm spots
 					<br><em>One mile away</em>
 				</p>
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-3">
 				<img src="http://i.imgur.com/fWuAnsA.jpg">
 				<h4>Sindy with a S</h4>
 				<p>Very tiny fluff of life
